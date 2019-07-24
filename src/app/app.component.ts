b258b60d664b12711f0f29f9d5a8f541b2e31cd7
unknown <diogo.torres.30@gmail.com>
2019-07-24 01:37:19 +0100

    quill!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rre';
+
+  editorForm: FormGroup;
+
+  ngOnInit(){
+    this.editorForm = new FormGroup({
+      'editor': new FormControl(null)
+    })
+  }
 }
