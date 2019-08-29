2e378fea5da10fabb262633d036bb382ec61e622
Diogo Torres <diogo.torres.30@gmail.com>
2019-08-29 14:13:01 +0100

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,26 @@ export class AppComponent implements OnInit {
 
   editorForm: FormGroup;
 
+  editorContent: string;
+
+  editorStyle ={
+    height: '300px',
+  }
+
   ngOnInit(){
     this.editorForm = new FormGroup({
       'editor': new FormControl(null)
     })
+  }
+
+  onSubmit(){
+    this.editorContent = this.editorForm.get('editor').value;
+    console.log(this.editorForm.get('editor').value);
+  }
+
+  maxLength(e){
+    if(e.editor.getLength() > 1000){
+      e.editor.deleteText(1000, e.editor.getLength());
+    }
   }
 }
